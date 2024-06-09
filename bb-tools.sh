e4492becd1414ddb391a82c3194e100b3e96bfad
Nicolas Montalbano <nicolasignacio.montalbano@gmail.com>
2024-06-09 17:29:02 -0300

    Se agregan validaciones y herramientas

--- bb-tools.sh
+++ bb-tools.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Función para agregar un directorio al PATH si no está presente y existe
+add_to_path_if_not_exists() {
+    local dir=$1
+    if [[ -d "$dir" && ":$PATH:" != *":$dir:"* ]]; then
+        echo "Agregando $dir al PATH..."
+        export PATH="$PATH:$dir"
+    fi
+}
+
 # Función para verificar si Go está instalado y, si no lo está, instalarlo
 install_go() {
     if ! command -v go &>/dev/null; then
@@ -26,12 +35,29 @@ install_pip3() {
     fi
 }
 
+# Función para verificar si una herramienta está instalada
+tool_is_installed() {
+    local tool_name=$1
+    if command -v "$tool_name" &>/dev/null; then
+        echo "$tool_name ya está instalado."
+        return 0  # Éxito
+    else
+        echo "$tool_name no está instalado."
+        return 1  # Error
+    fi
+}
+
 # Función para leer el archivo de configuración y realizar las instalaciones
 install_tools_from_config() {
     # Lee el archivo de configuración línea por línea
     while IFS='=' read -r tool command url; do
         # Verifica si la línea no está vacía y no es un comentario
         if [[ ! -z "$tool" && ! "$tool" =~ ^\s*# ]]; then
+            # Verifica si la herramienta ya está instalada
+            if tool_is_installed "$tool"; then
+                continue  # Salta a la próxima herramienta
+            fi
+            
             echo "Instalando $tool desde $url..."
             # Ejecuta el comando de instalación
             $command
@@ -47,4 +73,5 @@ install_tools_from_config() {
 # Llamadas a las funciones
 install_go
 install_pip3
+add_to_path_if_not_exists "$HOME/go/bin"
 install_tools_from_config
