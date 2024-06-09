79b75575d4945867f17ed8be7fc94f7beb82fb0a
Nicolas Montalbano <nicolasignacio.montalbano@gmail.com>
2024-06-09 18:51:31 -0300

    Se agregan validaciones para path y herramientas existentes

--- bb-tools.sh
+++ bb-tools.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Lista para almacenar las rutas del PATH ya agregadas
+added_paths=()
+
 # Función para agregar un directorio al PATH si no está presente y existe
 add_to_path_if_not_exists() {
     local dir=$1
     if [[ -d "$dir" ]]; then
-        echo "Agregando $dir al PATH en ~/.zshrc..."
-        # Verificar si el directorio ya está en el PATH en .zshrc
-        if ! grep -qE ":$dir(:|\$)" ~/.zshrc; then
+        # Verificar si la ruta ya está en el PATH en .zshrc
+        if grep -qE ":$dir(:|\$)" ~/.zshrc; then
+            echo "$dir ya está en el PATH en ~/.zshrc."
+        elif [[ " ${added_paths[@]} " =~ " $dir " ]]; then
+            echo "$dir ya ha sido procesado."
+        else
+            added_paths+=("$dir")  # Almacena la ruta procesada
+            echo "Agregando $dir al PATH en ~/.zshrc..."
             sudo echo "export PATH=\$PATH:$dir" >> ~/.zshrc
         fi
+    else
+        echo "$dir no existe o no es un directorio."
     fi
 }
 
@@ -38,10 +48,13 @@ install_pip3() {
     fi
 }
 
+# Lista para almacenar el estado de instalación de cada herramienta
+installed_tools=()
+
 # Función para verificar si una herramienta está instalada
 tool_is_installed() {
     local tool_name=$1
-    if command -v "$tool_name" &>/dev/null; then
+    if command -v $tool_name &>/dev/null; then
         echo "$tool_name ya está instalado."
         return 0  # Éxito
     else
@@ -51,17 +64,19 @@ tool_is_installed() {
 }
 
 # Función para leer el archivo de configuración y realizar las instalaciones
-# Función para leer el archivo de configuración y realizar las instalaciones
 install_tools_from_config() {
     local config_file="tools.conf"
     # Lee el archivo de configuración línea por línea
     while IFS='=' read -r tool command url; do
         # Verifica si la línea no está vacía y no es un comentario
         if [[ ! -z "$tool" && ! "$tool" =~ ^\s*# ]]; then
-            # Verifica si la herramienta ya está instalada
-            if tool_is_installed "$tool"; then
+            # Verifica si la herramienta ya ha sido instalada
+            if command -v $tool &>/dev/null; then
                 echo "$tool ya está instalado."
+            elif [[ " ${installed_tools[@]} " =~ " $tool " ]]; then
+                echo "$tool ya ha sido procesado."
             else
+                installed_tools+=("$tool")  # Almacena la herramienta procesada
                 echo "Instalando $tool desde $url..."
                 # Ejecuta el comando de instalación
                 $command
